1aea2c18108a7a60a4e8cfe723a953706fb49639
Douglas Neves Schuelter <douglas.schuelter@texoit.com>
2015-09-13 10:59:32 -0300

    melhoria da interface e adição do sistema de rating star.

--- src/main/webapp/resources/index/controller.js
+++ src/main/webapp/resources/index/controller.js
@@ -23,12 +23,45 @@ var controller = {
 	loadLevelForm : function() {
 		var html = Mustache.render($(".level-template").html(), this.user)
 		$('#level-form').append(html)
+		this.startsProperties()
+	},
+	
+	startsProperties: function(){
+		$('input[type="number"]').rating({
+			clearCaption: 'Nota 0',
+			clearCaptionClass: 'label label-warning',
+			starCaptions: {
+			    0: 'Nota 0',
+			    1: 'Nota 1',
+			    2: 'Nota 2',
+			    3: 'Nota 3',
+			    4: 'Nota 4',
+			    5: 'Nota 5',
+			    6: 'Nota 6',
+			    7: 'Nota 7',
+			    8: 'Nota 8',
+			    9: 'Nota 9',
+			    10: 'Nota 10'
+			},
+			starCaptionClasses: {
+			    0: 'label label-warning',
+			    1: 'label label-warning',
+			    2: 'label label-warning',
+			    3: 'label label-warning',
+			    4: 'label label-warning',
+			    5: 'label label-warning',
+			    6: 'label label-warning',
+			    7: 'label label-success',
+			    8: 'label label-success',
+			    9: 'label label-success',
+			    10: 'label label-success'
+			}
+			})
 	},
 
 	finish : function() {
 		for ( var n in this.user.abilites) {
-			console.log(n)
-			this.user.abilites[n].value = $('#level_'+this.user.abilites[n].skill.id).find('input:checked').val()
+			this.user.abilites[n].value = $('#level_'+this.user.abilites[n].skill.id).val()
 		}
 		this.sendInformation()
 	},
